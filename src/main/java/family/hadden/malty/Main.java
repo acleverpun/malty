a4372d8f934ebfa46c39f0d3829c4238371f0816
Ross Hadden <rosshadden@gmail.com>
2020-03-16 03:10:09 -0400

    Renamed main mod file to... well... main.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/main/java/family/hadden/malty/Main.java
+++ src/main/java/family/hadden/malty/Main.java
@@ -20,11 +20,11 @@ import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("malty")
-public class Malty {
+public class Main {
 	// Directly reference a log4j logger.
-	private static final Logger LOGGER = LogManager.getLogger();
+	private static final Logger log = LogManager.getLogger();
 
-	public Malty() {
+	public Main() {
 		// Register the setup method for modloading
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 		// Register the enqueueIMC method for modloading
@@ -40,23 +40,23 @@ public class Malty {
 
 	private void setup(final FMLCommonSetupEvent event) {
 		// some preinit code
-		LOGGER.info("HELLO FROM PREINIT");
-		LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+		log.info("HELLO FROM PREINIT");
+		log.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
 	}
 
 	private void doClientStuff(final FMLClientSetupEvent event) {
 		// do something that can only be done on the client
-		LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
+		log.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
 	}
 
 	private void enqueueIMC(final InterModEnqueueEvent event) {
 		// some example code to dispatch IMC to another mod
-		InterModComms.sendTo("malty", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+		InterModComms.sendTo("malty", "helloworld", () -> { log.info("Hello world from the MDK"); return "Hello world";});
 	}
 
 	private void processIMC(final InterModProcessEvent event) {
 		// some example code to receive and process InterModComms from other mods
-		LOGGER.info("Got IMC {}", event.getIMCStream().
+		log.info("Got IMC {}", event.getIMCStream().
 				map(m->m.getMessageSupplier().get()).
 				collect(Collectors.toList()));
 	}
@@ -65,7 +65,7 @@ public class Malty {
 	@SubscribeEvent
 	public void onServerStarting(FMLServerStartingEvent event) {
 		// do something when the server starts
-		LOGGER.info("HELLO from server starting");
+		log.info("HELLO from server starting");
 	}
 
 	// You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
@@ -73,9 +73,9 @@ public class Malty {
 	@Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
 	public static class RegistryEvents {
 		@SubscribeEvent
-		public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+		public static void onBlocksRegistry(final RegistryEvent.Register<Block> event) {
 			// register a new block here
-			LOGGER.info("HELLO from Register Block");
+			log.info("HELLO from Register Block");
 		}
 	}
 }
