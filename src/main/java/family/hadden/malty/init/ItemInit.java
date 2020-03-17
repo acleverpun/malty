d104d1137209bf3402dc8740406761794d418a7b
Ross Hadden <rosshadden@gmail.com>
2020-03-17 01:00:48 -0400

    Made plurality consistent.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/main/java/family/hadden/malty/init/ItemInit.java
+++ src/main/java/family/hadden/malty/init/ItemInit.java
@@ -2,7 +2,6 @@ package family.hadden.malty.init;
 
 import family.hadden.malty.Main;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -12,15 +11,9 @@ import net.minecraftforge.registries.ObjectHolder;
 @Mod.EventBusSubscriber(modid = Main.modId, bus = Bus.MOD)
 @ObjectHolder(Main.modId)
 public class ItemInit {
-	public static Item sturk;
-
 	@SubscribeEvent
 	public static void registerItems(final RegistryEvent.Register<Item> event) {
-		sturk = new Item(new Item.Properties().group(ItemGroup.MISC))
-			.setRegistryName("sturk");
-
 		event.getRegistry().registerAll(
-			sturk
 		);
 	}
 }
