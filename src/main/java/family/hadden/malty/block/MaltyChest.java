8ac4baa7c6b7def3629964bd43b164b0b331ac60
Ross Hadden <rosshadden@gmail.com>
2020-03-18 17:01:15 -0400

    Got tile entity created and working.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/main/java/family/hadden/malty/block/MaltyChest.java
+++ src/main/java/family/hadden/malty/block/MaltyChest.java
@@ -1,11 +1,24 @@
 package family.hadden.malty.block;
 
+import javax.annotation.Nullable;
+
+import family.hadden.malty.Main;
 import family.hadden.malty.init.TileEntityTypes;
+import family.hadden.malty.tileEntity.MaltyChestTileEntity;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fml.DistExecutor;
 
 public class MaltyChest extends Block {
 	public MaltyChest() {
@@ -13,15 +26,30 @@ public class MaltyChest extends Block {
 	}
 
 	@Override
-	public boolean hasTileEntity() {
+	public boolean hasTileEntity(BlockState state) {
 		return true;
 	}
 
+	@Nullable
 	@Override
 	public TileEntity createTileEntity(final BlockState state, final IBlockReader world) {
 		return TileEntityTypes.maltyChest.get().create();
 	}
 
-	// @Override
-	// public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {}
+	@Override
+	public ActionResultType onBlockActivated(final BlockState state, final World world, final BlockPos pos, final PlayerEntity player, final Hand hand, final BlockRayTraceResult hit) {
+		DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> openGui(world, pos));
+		return ActionResultType.SUCCESS;
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	private void openGui(final World world, final BlockPos pos) {
+		if (world.isRemote) {
+			final TileEntity tileEntity = world.getTileEntity(pos);
+			Main.log.debug("Wait for it...");
+			if (tileEntity instanceof MaltyChestTileEntity) {
+				Main.log.debug("Yeppers");
+			}
+		}
+	}
 }
--- src/main/java/family/hadden/malty/tileEntity/MaltyChestTileEntity.java
+++ src/main/java/family/hadden/malty/tileEntity/MaltyChestTileEntity.java
@@ -1,10 +1,25 @@
 package family.hadden.malty.tileEntity;
 
+import family.hadden.malty.init.Blocks;
 import family.hadden.malty.init.TileEntityTypes;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 
-public class MaltyChestTileEntity extends TileEntity {
+public class MaltyChestTileEntity extends TileEntity /* implements INamedContainerProvider */ {
 	public MaltyChestTileEntity() {
 		super(TileEntityTypes.maltyChest.get());
 	}
+
+	public ITextComponent getDisplayName() {
+		return new TranslationTextComponent(Blocks.maltyChest.get().getTranslationKey());
+	}
+
+	// public Container createMenu(final int windowId, final PlayerInventory inventory, final PlayerEntity player) {
+	// 	return new MaltyChestContainer(windowId, inventory, this);
+	// }
 }
