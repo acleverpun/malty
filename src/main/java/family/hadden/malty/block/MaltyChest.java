1130f19ee512f4f1d87395c345901527ed60c28f
Ross Hadden <rosshadden@gmail.com>
2020-03-18 12:16:16 -0400

    Added malty chest tile entity.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/main/java/family/hadden/malty/block/MaltyChest.java
+++ src/main/java/family/hadden/malty/block/MaltyChest.java
@@ -1,5 +1,6 @@
 package family.hadden.malty.block;
 
+import family.hadden.malty.init.TileEntityTypes;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
@@ -11,10 +12,15 @@ public class MaltyChest extends Block {
 		super(Block.Properties.create(Material.ROCK));
 	}
 
-	// @Override
-	// public TileEntity createTileEntity(BlockState state, IBlockReader world) {
-	// 	return new TileEntity();
-	// }
+	@Override
+	public boolean hasTileEntity() {
+		return true;
+	}
+
+	@Override
+	public TileEntity createTileEntity(final BlockState state, final IBlockReader world) {
+		return TileEntityTypes.maltyChest.get().create();
+	}
 
 	// @Override
 	// public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {}
---
+++ src/main/java/family/hadden/malty/tileEntity/MaltyChestTileEntity.java
@@ -0,0 +1,10 @@
+package family.hadden.malty.tileEntity;
+
+import family.hadden.malty.init.TileEntityTypes;
+import net.minecraft.tileentity.TileEntity;
+
+public class MaltyChestTileEntity extends TileEntity {
+	public MaltyChestTileEntity() {
+		super(TileEntityTypes.maltyChest.get());
+	}
+}
