d8ad81042973bfd0636abceffadfd097384eca72
Ross Hadden <rosshadden@gmail.com>
2020-03-18 23:42:24 -0400

    Dynamically create and register BlockItems for all Blocks.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/main/java/family/hadden/malty/init/Items.java
+++ src/main/java/family/hadden/malty/init/Items.java
@@ -3,15 +3,31 @@ package family.hadden.malty.init;
 import family.hadden.malty.Main;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.IForgeRegistry;
 
+@EventBusSubscriber(modid = Main.modId, bus = EventBusSubscriber.Bus.MOD)
 public final class Items {
 	public static final DeferredRegister<Item> registry = new DeferredRegister<>(ForgeRegistries.ITEMS, Main.modId);
 
-	public static final RegistryObject<Item> maltyChest = registry.register(
-		"malty-chest",
-		() -> new BlockItem(Blocks.maltyChest.get(), new Item.Properties().group(Main.tab))
-	);
+	@SubscribeEvent
+	public static void onRegisterItems(final RegistryEvent.Register<Item> event) {
+		final IForgeRegistry<Item> registry = event.getRegistry();
+
+		// Automatically register BlockItems for all Blocks
+		Blocks.registry.getEntries().stream()
+			.map(RegistryObject::get)
+			.forEach(block -> {
+				Main.log.debug("onRegisterItems " + block.getRegistryName());
+				final Item.Properties properties = new Item.Properties().group(Main.tab);
+				final BlockItem blockItem = new BlockItem(block, properties);
+				blockItem.setRegistryName(block.getRegistryName());
+				registry.register(blockItem);
+			});
+	}
 }
